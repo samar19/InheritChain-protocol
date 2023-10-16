b09b965df475072001427d8ff18509860a5c1f09
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-10-16 22:16:04 +0300

    edit readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.9.0", // Choose the appropriate version for your contract
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/v1/cbf49a46f64f11243f0d05507ecb0be9484fdcb6", // Replace with your Mumbai testnet RPC URL
+      accounts: [process.env.PRIVATE_KEY], // Add your private key as an environment variable for security
+    },
+  },
+ 
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai:"C1YKEWTCTFS3TS4U1PNCCHZ5FNHI9XV6UB"
+    }
+  }
 };
